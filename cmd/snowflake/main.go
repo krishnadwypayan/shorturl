4b3a8e138498c8d26247df81fcf1921918f7c32e
Krishna Kota <kota_k@apple.com>
2025-06-08 02:22:02 +0530

    print banner

--- cmd/snowflake/main.go
+++ cmd/snowflake/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/krishnadwypayan/shorturl/internal/routes"
 	"github.com/krishnadwypayan/shorturl/internal/snowflake"
 )
 
 func main() {
+	printBanner()
 	generator := snowflake.NewGenerator(1)
 	r := gin.Default()
 	routes.RegisterSnowflakeRoutes(r, generator)
 	r.Run(":8080")
+}
+
+func printBanner() {
+	fmt.Print(`
+███████╗███╗   ██╗ ██████╗ ██╗    ██╗███████╗██╗      █████╗ ██╗  ██╗███████╗
+██╔════╝████╗  ██║██╔═══██╗██║    ██║██╔════╝██║     ██╔══██╗██║ ██╔╝██╔════╝
+███████╗██╔██╗ ██║██║   ██║██║ █╗ ██║█████╗  ██║     ███████║█████╔╝ █████╗  
+╚════██║██║╚██╗██║██║   ██║██║███╗██║██╔══╝  ██║     ██╔══██║██╔═██╗ ██╔══╝  
+███████║██║ ╚████║╚██████╔╝╚███╔███╔╝██║     ███████╗██║  ██║██║  ██╗███████╗
+╚══════╝╚═╝  ╚═══╝ ╚═════╝  ╚══╝╚══╝ ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝                      
+`)
 }
